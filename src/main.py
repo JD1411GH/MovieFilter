d2109ccc7ecf11264d2d2726ce4a3e912dfa6fdf
Jayanta <Jayanta.Debnath@in.bosch.com>
2025-05-28 17:37:56 +0530

    database read operations

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import json
 import os
 import dataset
-from openpyxl import load_workbook
 
 curdir = os.path.dirname(__file__)
 rootdir = os.path.dirname(curdir)
@@ -13,23 +12,10 @@ def main():
     db = dataset.connect(f'sqlite:///{config["dbfile"]}')
     table = db['movies']
 
-    workbook = load_workbook(filename='LockerDB.xlsx')
-    sheet = workbook.active
-    headers = {header: idx for idx, header in enumerate(next(sheet.iter_rows(values_only=True)), 1)}
-    for row in sheet.iter_rows(min_row=2, values_only=True):
-        table.insert({
-            'rel_path': row[headers['rel_path'] - 1],
-            'movie_rating': row[headers['movie_rating'] - 1],
-            'actor_rating': row[headers['actor_rating'] - 1],
-            'actor': row[headers['actor'] - 1],
-            'category': row[headers['category'] - 1],
-            'studio': row[headers['studio'] - 1],
-            'last_played': "",
-            'playcount': row[headers['playcount'] - 1]
-            })
-
-
-    
+    select = table.find(actor_rating=6.0)
+    for row in select:
+        print(row['actor'])
+        break
 
 
 if __name__ == "__main__":
